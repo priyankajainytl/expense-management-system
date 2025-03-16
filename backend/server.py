15e5a485b64df0c59ddfc8c47efb351f859fd1bc
Priyanka <priyankajainytl@gmail.com>
2025-03-16 12:12:51 -0700

    Add expense model

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI
 from datetime import date
+from typing import List
+from pydantic import BaseModel
 import db_helper
 app = FastAPI()
 
+
+class Expense(BaseModel):
+    id: int
+    amount: float
+    category: str
+    notes: str
 
 @app.get("/")
 def hello():
    return "Hello there"
 
-@app.get("/expenses/{expense_date}")
+@app.get("/expenses/{expense_date}", response_model=List[Expense])
+def get_expenses(expense_date:date):
+   expenses=db_helper.fetch_expenses_for_date(expense_date)
+   return expenses
+
+@app.get("/expenses/v1/{expense_date}")
 def get_expenses(expense_date:date):
    expenses=db_helper.fetch_expenses_for_date(expense_date)
    return expenses
