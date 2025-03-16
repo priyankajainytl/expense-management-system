37c2383968fd1d59a031635c99aa2f0e4df33400
Priyanka <priyankajainytl@gmail.com>
2025-03-16 12:12:51 -0700

    Initial commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from datetime import datetime
+import requests
+
+API_URL="https://localhost:8000"
+
+st.title("Expense Managment System")
+
+tab1,tab2 =st.tabs(["Add/Update","Analytics"])
+with tab1:
+    selected_date=st.date_input("Enter Date",datetime(2024,8,1),label_visibility="collapsed")
+    response=requests.get(f"{API_URL}/expenses/{selected_date}")
+    if response.status_code==200:
+        existing_expenses=response.json()
+        st.write(existing_expenses)
+    else:
+        st.error("falied to retrieve response")
+        existing_expenses=[]
