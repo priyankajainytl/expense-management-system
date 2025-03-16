56aa623c73e312742669a8c1bedaa8eeb075f0c8
Priyanka <priyankajainytl@gmail.com>
2025-03-16 12:12:51 -0700

    Added tests - all passing

--- tests/backend/test_db_helper.py
+++ tests/backend/test_db_helper.py
@@ -1,5 +1,3 @@
-import os, sys
-
 from backend import db_helper
 
 def test_fetch_expenses_for_date_aug_15():
@@ -11,12 +9,12 @@ def test_fetch_expenses_for_date_aug_15():
     assert expenses[0]["notes"] == "Bought potatoes"
 
 
-##def test_fetch_expenses_for_date_invalid_date():
-    #expenses = db_helper.fetch_expenses_for_date("9999-08-15")
+def test_fetch_expenses_for_date_invalid_date():
+    expenses = db_helper.fetch_expenses_for_date("9999-08-15")
 
-#    assert len(expenses) == 0
+    assert len(expenses) == 0
 
 
-#def test_fetch_expense_summary_invalid_range():
- #   summary = db_helper.fetch_expense_summary("2099-01-01", "2099-12-31")
-  #  assert len(summary) == 0
+def test_fetch_expense_summary_invalid_range():
+    summary = db_helper.fetch_expense_summary("2099-01-01", "2099-12-31")
+    assert len(summary) == 0
