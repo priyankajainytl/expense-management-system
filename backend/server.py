1e1967789edac9ca3da3de36291a7d5ef46f10e9
Priyanka <priyankajainytl@gmail.com>
2025-03-16 12:12:51 -0700

    Adding separate files for frontend. Adding POST method for viewing analytics by category

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from datetime import date
 from typing import List
 from pydantic import BaseModel
@@ -11,6 +11,11 @@ class Expense(BaseModel):
     amount: float
     category: str
     notes: str
+
+# Base model for date range
+class DateRange(BaseModel):
+    start_date: date
+    end_date: date
 
 @app.get("/")
 def hello():
@@ -30,3 +35,22 @@ def add_or_update_expense(expense_date: date, expenses:List[Expense]):
         db_helper.insert_expense(expense_date, expense.amount, expense.category, expense.notes)
 
     return {"message": "Expenses updated successfully"}
+
+# Endpoint to show expenses by category
+@app.post("/analytics/")
+def get_analytics(date_range: DateRange):
+    data = db_helper.fetch_expense_summary(date_range.start_date, date_range.end_date)
+    if data is None:
+        raise HTTPException(status_code=500, detail="Failed to retrieve expense summary from the database.")
+
+    total = sum([row['total'] for row in data])
+
+    breakdown = {}
+    for row in data:
+        percentage = (row['total']/total)*100 if total != 0 else 0
+        breakdown[row['category']] = {
+            "total": row['total'],
+            "percentage": percentage
+        }
+
+    return breakdown
